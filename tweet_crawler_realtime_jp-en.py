2d9cd3856a5256955594dd4585dc7d16740c137e
lemonaz5 <gam_wannita@hotmail.com>
2018-05-30 17:44:54 +0900

    jp crawler

--- tweet_crawler_realtime_jp-en.py
+++ tweet_crawler_realtime_jp-en.py
@@ -20,7 +20,7 @@ api = tweepy.API(auth)
 #query = ["Bangkok"]
 label = "BKK_th-en" 
 languages = ["en", "th"]
-longlat = [100.484605,13.685780,100.584605,13.785780] 
+longlat = [135.685300,34.931461,135.785300,35.031461] 
 # ------- EDIT -------- #
 tweet_list=set()
 
@@ -31,15 +31,15 @@ def writefile(tweet):
         tweet_list.add(tweet_id)
         tweet_user_id = tweet.user.id_str
         tweet_user = tweet.user.screen_name
-        tweet_text = str(tweet.text).encode("CP874", "ignore").decode("CP874")
+        tweet_text = str(tweet.text).encode("CP932", "ignore").decode("CP932")
         tweet_text = tweet_text.replace('\n', ' ')
-        tweet_time = str(tweet.created_at).encode("CP874", "ignore").decode("CP874")
-        tweet_lang = str(tweet.lang).encode("CP874", "ignore").decode("CP874")
+        tweet_time = str(tweet.created_at).encode("CP932", "ignore").decode("CP932")
+        tweet_lang = str(tweet.lang).encode("CP932", "ignore").decode("CP932")
         tweet_reply_id = tweet.in_reply_to_status_id_str if tweet.in_reply_to_status_id_str != None else '-1'
         tweet_reply_user_id = tweet.in_reply_to_user_id_str if tweet.in_reply_to_user_id_str != None else '-1'
         tweet_reply_username = tweet.in_reply_to_screen_name if tweet.in_reply_to_screen_name != None else '-1'
         tweet_coord = tweet.coordinates
-        geo = str(tweet_coord).encode("CP874", "ignore").decode("CP874")
+        geo = str(tweet_coord).encode("CP932", "ignore").decode("CP932")
         location = "none"
         locat_sw = '-1'
         locat_ne = '-1'
@@ -53,33 +53,12 @@ def writefile(tweet):
             longitude = latlong[0]
             latitude = latlong[1]
         if tweet.place != None:
-            location = str(tweet.place.name).encode("CP874", "ignore").decode("CP874")
-            locat_sw = str(tweet.place.bounding_box.coordinates[0][0])[1:-1].encode("CP874", "ignore").decode("CP874")
-            locat_ne = str(tweet.place.bounding_box.coordinates[0][2])[1:-1].encode("CP874", "ignore").decode("CP874")
+            location = str(tweet.place.name).encode("CP932", "ignore").decode("CP932")
+            locat_sw = str(tweet.place.bounding_box.coordinates[0][0])[1:-1].encode("CP932", "ignore").decode("CP932")
+            locat_ne = str(tweet.place.bounding_box.coordinates[0][2])[1:-1].encode("CP932", "ignore").decode("CP932")
 
         #tweet_with_time_and_geo = tweet_user_id + "\t" + tweet_user + "\t" + tweet_id + "\t" + tweet_text + "\t" + tweet_time + "\t" + latitude + "\t" + longitude + "\t" + location + "\t" + locat_sw + "\t" + locat_ne + "\t" + tweet_lang + "\n" 
         tweet_with_time_and_geo = tweet_user_id + "\t" + tweet_user + "\t" + tweet_id + "\t" + tweet_text + "\t" + tweet_time + "\t" + latitude + "\t" + longitude + "\t" + location + "\t" + locat_sw + "\t" + locat_ne + "\t" + tweet_reply_id + "\t" + tweet_reply_user_id + "\t" + tweet_reply_username + "\t" + tweet_lang + "\n" 
 
-        f.write(tweet_with_time_and_geo.encode("CP874", "ignore").decode("CP874"))
+        f.write(tweet_with_time_and_geo.encode("CP932", "ignore").decode("CP932"))
     f.close()
-
-
-class MyStreamListener(tweepy.StreamListener):
-
-    def on_status(self, status):
-        print("text: ", str(status.text).encode("CP874", "ignore").decode("CP874"))
-        writefile(status)
-
-    def on_error(self, status_code):
-        if status_code == 420:
-            #returning False in on_error disconnects the stream
-            return 
-        else:
-            print('ok')
-
-        # returning non-False reconnects the stream, with backoff.
-
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-
-myStream.filter(locations=longlat, languages=languages, async=True)
