02940dabf368541cf9d6041a79b8fc41ad04893e
aroundy <gam_wannita@hotmail.com>
2018-05-30 21:17:46 +0700

    fixed jp crawler

--- tweet_crawler_realtime_jp-en.py
+++ tweet_crawler_realtime_jp-en.py
@@ -18,16 +18,16 @@ api = tweepy.API(auth)
 # ------- EDIT -------- #
 #query1 = 'こんにちわ'
 #query = ["Bangkok"]
-label = "BKK_th-en" 
-languages = ["en", "th"]
-longlat = [135.685300,34.931461,135.785300,35.031461] 
+label = "KYT_ja-en"
+languages = ["en", "ja"]
+longlat = [135.685300,34.931461,135.785300,35.031461]
 # ------- EDIT -------- #
 tweet_list=set()
 
 def writefile(tweet):
     f = open(label+'_tweets.txt','a',encoding='utf-8')
     tweet_id = tweet.id_str
-    if tweet_id not in tweet_list: 
+    if tweet_id not in tweet_list:
         tweet_list.add(tweet_id)
         tweet_user_id = tweet.user.id_str
         tweet_user = tweet.user.screen_name
@@ -45,7 +45,7 @@ def writefile(tweet):
         locat_ne = '-1'
         latitude = '-1'
         longitude = '-1'
-        
+
         if len(geo) > 10:
             ind = geo.find("[")
             ind2 = geo.find("]")
@@ -57,8 +57,27 @@ def writefile(tweet):
             locat_sw = str(tweet.place.bounding_box.coordinates[0][0])[1:-1].encode("CP932", "ignore").decode("CP932")
             locat_ne = str(tweet.place.bounding_box.coordinates[0][2])[1:-1].encode("CP932", "ignore").decode("CP932")
 
-        #tweet_with_time_and_geo = tweet_user_id + "\t" + tweet_user + "\t" + tweet_id + "\t" + tweet_text + "\t" + tweet_time + "\t" + latitude + "\t" + longitude + "\t" + location + "\t" + locat_sw + "\t" + locat_ne + "\t" + tweet_lang + "\n" 
-        tweet_with_time_and_geo = tweet_user_id + "\t" + tweet_user + "\t" + tweet_id + "\t" + tweet_text + "\t" + tweet_time + "\t" + latitude + "\t" + longitude + "\t" + location + "\t" + locat_sw + "\t" + locat_ne + "\t" + tweet_reply_id + "\t" + tweet_reply_user_id + "\t" + tweet_reply_username + "\t" + tweet_lang + "\n" 
+        #tweet_with_time_and_geo = tweet_user_id + "\t" + tweet_user + "\t" + tweet_id + "\t" + tweet_text + "\t" + tweet_time + "\t" + latitude + "\t" + longitude + "\t" + location + "\t" + locat_sw + "\t" + locat_ne + "\t" + tweet_lang + "\n"
+        tweet_with_time_and_geo = tweet_user_id + "\t" + tweet_user + "\t" + tweet_id + "\t" + tweet_text + "\t" + tweet_time + "\t" + latitude + "\t" + longitude + "\t" + location + "\t" + locat_sw + "\t" + locat_ne + "\t" + tweet_reply_id + "\t" + tweet_reply_user_id + "\t" + tweet_reply_username + "\t" + tweet_lang + "\n"
 
         f.write(tweet_with_time_and_geo.encode("CP932", "ignore").decode("CP932"))
     f.close()
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print("text: ", status.text)
+        writefile(status)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_error disconnects the stream
+            return
+
+        # returning non-False reconnects the stream, with backoff.
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+
+myStream.filter(locations=longlat, languages=languages, async=True)
+
